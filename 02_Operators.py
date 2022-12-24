7506a8b85624d97e1136e0f58fc4f68578b77266
zjooie <121341380+zjooie@users.noreply.github.com>
2022-12-24 17:30:39 +0530

    Python Operator
    Operators are use to perform operations on values and variables, these are standard symbols used for the purpose of logical and arithmetic operations.
    
    There are four types of operators.
    
    1. Arithmetic Operator: are used to performing mathematical operations like addition, subtraction, multiplication, and division.
    2. Assignment Operator : are used to assign values to the variables (=,+=,-=,*=,/=,//=,%=,&=,|=,>>=,<<=).
    3. Logical Operator : Perform Logical AND, Logical OR, and Logical NOT operations. It is used to combine conditional statements. 
    4. Comparison Operator : are use to compare two values (==,!=,<=,>=,>,<).

---
+++ 02_Operators.py
@@ -0,0 +1,36 @@
+a= 3
+b=4
+
+#Arithmatic Operators
+print("The add of 3+4 is:", a+b)
+print("The add of 3-4 is:", a-b)
+print("The add of 3*4 is:", a*b)
+print("The add of 3/4 is:", a/b)
+
+# Assignment Operator
+a=25
+a -= 3
+print("-=",a)
+a += 21
+print("+=",a)
+a *= 21
+print("*=",a)
+a /= 21
+print("/=",a)
+
+#Comparision Operator
+
+b=(4<7)
+print("<",b)
+
+b=(4>7)
+print(">",b)
+
+# Logical Operator
+
+bool1 = True
+bool2 = False
+
+print("Value of Bool1 and Bool2",(bool1 and bool2))
+print("Value of Bool1 or Bool2",(bool1 or bool2))
+print("Value of not Bool2",(not bool2))
